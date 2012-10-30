0f047b5b1e32c6096f8cb2aa27eb5cae31eba4b4
valabau <valabau@8b0d7800-f05a-0410-912a-ed64b2e63916>
2012-10-30 10:42:33 +0000

    Add HTR interface
    git-svn-id: https://projects.iti.upv.es/svn/prhlt/software/casmacat-server-library/trunk@6376 8b0d7800-f05a-0410-912a-ed64b2e63916

---
+++ src/include/casmacat/IHtrEngine.h
@@ -0,0 +1,81 @@
+/*
+ *   Copyright 2012, valabau
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ * 
+ * IHtrEngine.h
+ *
+ *  Created on: 15/10/2012
+ *      Author: valabau
+ */
+
+#ifndef CASMACAT_IHTRENGINE_H_
+#define CASMACAT_IHTRENGINE_H_
+
+#include <string>
+#include <vector>
+#include <casmacat/IPluginFactory.h>
+
+
+namespace casmacat {
+
+/**
+ * Interface for Machine Translation plug-ins
+ */
+
+  class IHtrSession {
+  public:
+    typedef enum { PEN_UP, PEN_DOWN } stroke_type_t;
+
+    virtual ~IHtrSession() {};
+
+    /* Set partial validation of a translation */
+    virtual void addPoint(float x, float y, stroke_type_t type) = 0;
+
+    /* Set prefix of a translation */
+    virtual void decode(std::vector<std::string> &corrected_suffix_out) = 0;
+  };
+
+  class IHtrEngine {
+  public:
+    virtual ~IHtrEngine() {};
+
+    /**
+     * create new HTR session from a partial validation of a translation
+     */
+    virtual IHtrSession *createSession(const std::vector<std::string> &partial_translation,
+                                       const std::vector<bool> &validated
+                                      ) = 0;
+
+    /**
+     * create new HTR session from a validated prefix and not validated suffix
+     */
+    virtual IHtrSession *createSessionFromPrefix(const std::vector<std::string> &prefix,
+                                                 const std::vector<std::string> &suffix,
+                                                 const bool last_token_is_partial
+                                                ) = 0;
+
+    /**
+     * delete HTR session
+     */
+    virtual void deleteSession(IHtrEngine *session) = 0;
+
+  };
+
+  typedef IPluginFactory<IHtrEngine> IHtrFactory;
+}
+
+
+
+#endif /* CASMACAT_IHTRENGINE_H_ */
