d48710e1810a3c0a3a2e81e9e73479e145953dfb
valabau <valabau@8b0d7800-f05a-0410-912a-ed64b2e63916>
2013-10-02 09:05:58 +0000

    Make casmacat htr server more configurable
    git-svn-id: https://projects.iti.upv.es/svn/prhlt/software/casmacat-server-library/trunk@9076 8b0d7800-f05a-0410-912a-ed64b2e63916

--- server/casmacat-htr-server.py
+++ server/casmacat-htr-server.py
@@ -3,7 +3,7 @@
 
 import sys, traceback, os
 import datetime, time
-import random, math, codecs
+import random, math, codecs, collections
 
 try: import simplejson as json
 except ImportError: import json
@@ -128,15 +128,29 @@ ROOT = os.path.normpath(os.path.dirname(__file__))
 def filter_utf8(string):
   return string.encode('utf-8')
 
+def convert(data):
+    if isinstance(data, unicode):
+        return filter_utf8(data)
+    elif isinstance(data, basestring):
+        return data
+    elif isinstance(data, collections.Mapping):
+        return dict(map(convert, data.iteritems()))
+    elif isinstance(data, collections.Iterable):
+        return type(data)(map(convert, data))
+    else:
+        return data
+
 def to_utf8(obj):
-  if obj == None:
-    return obj
-  elif isinstance(obj, basestring):
-    return filter_utf8(obj)
-  elif isinstance(obj, list): 
-    return [to_utf8(w) for w in obj]
-  print "Unknown type", type(obj), "for object", obj
-  raise "Unknown type"
+  return convert(obj)
+#def to_utf8(obj):
+#  if obj == None:
+#    return obj
+#  elif isinstance(obj, basestring):
+#    return filter_utf8(obj)
+#  elif isinstance(obj, list): 
+#    return [to_utf8(w) for w in obj]
+#  print "Unknown type", type(obj), "for object", obj
+#  raise "Unknown type"
 
 
 class HtrConnection(SocketConnection):
@@ -152,21 +166,21 @@ class HtrConnection(SocketConnection):
       source, target = to_utf8(data['source']), to_utf8(data['target'])
       caret_pos = data['caretPos']
       if self.htr_session: 
-        htr.deleteSession(self.htr_session)
+        models.htr.deleteSession(self.htr_session)
         self.htr_session = None
         self.strokes = None
 
       logger.log(DEBUG_LOG, str(caret_pos) + " @ " + target);
 
-      source_tok, source_seg = tokenizer.preprocess(source)
+      source_tok, source_seg = models.tokenizer.preprocess(source)
       print >> sys.stderr, "source", source
 
       prefix = target[:caret_pos] 
-      prefix_tok, prefix_seg = tokenizer.preprocess(prefix)
+      prefix_tok, prefix_seg = models.tokenizer.preprocess(prefix)
       print >> sys.stderr, "prefix", prefix
 
       suffix = target[caret_pos:] 
-      suffix_tok, suffix_seg = tokenizer.preprocess(suffix)
+      suffix_tok, suffix_seg = models.tokenizer.preprocess(suffix)
       print >> sys.stderr, "suffix", suffix 
 
       last_token_is_partial = False
@@ -175,8 +189,8 @@ class HtrConnection(SocketConnection):
       print >> sys.stderr, "last_token_is_partial", last_token_is_partial 
 
       start_time = datetime.datetime.now()
-      #self.htr_session = htr.createSessionFromPrefix(source_tok, prefix_tok, suffix_tok, last_token_is_partial)
-      self.htr_session = htr.createSessionFromPrefix([], [], [], False)
+      #self.htr_session = models.htr.createSessionFromPrefix(source_tok, prefix_tok, suffix_tok, last_token_is_partial)
+      self.htr_session = models.htr.createSessionFromPrefix([], [], [], False)
       self.strokes = []
       elapsed_time = datetime.datetime.now() - start_time
       obj = { 'elapsedTime': elapsed_time.total_seconds()*1000.0 }
@@ -200,14 +214,14 @@ class HtrConnection(SocketConnection):
             has_partial, partial_result_tok = self.htr_session.decodePartially()
             elapsed_time = datetime.datetime.now() - start_time
             if has_partial:
-              partial_result, partial_result_seg = tokenizer.postprocess(partial_result_tok)
+              partial_result, partial_result_seg = models.tokenizer.postprocess(partial_result_tok)
               print >> sys.stderr, "update", partial_result_tok
 
-              obj = { 'nbest': { 
+              obj = { 'nbest': [{ 
                         'text': partial_result, 
                         'textSegmentation': partial_result_seg,
                         'elapsedTime': elapsed_time.total_seconds()*1000.0
-                      }, 
+                      }], 
                       'elapsedTime': elapsed_time.total_seconds()*1000.0
                     }
 
@@ -225,20 +239,20 @@ class HtrConnection(SocketConnection):
           result_tok = self.htr_session.decode()
           elapsed_time = datetime.datetime.now() - start_time
           print >> sys.stderr, "change", result_tok
-          result, result_seg = tokenizer.postprocess(result_tok)
+          result, result_seg = models.tokenizer.postprocess(result_tok)
 
-          obj = { 'nbest': { 
+          obj = { 'nbest': [{ 
                     'text': result, 
                     'textSegmentation': result_seg,
                     'elapsedTime': elapsed_time.total_seconds()*1000.0
-                  }, 
+                  }], 
                   'elapsedTime': elapsed_time.total_seconds()*1000.0
                 }
 
           self.respond('endSessionResult', { 'errors': [], 'data': obj })
 
 
-          htr.deleteSession(self.htr_session)
+          models.htr.deleteSession(self.htr_session)
           self.htr_session = None
           dump_strokes(self.strokes)
           self.strokes = None
@@ -264,7 +278,7 @@ class HtrConnection(SocketConnection):
     @event
     def on_close(self):
       if self.htr_session: 
-        htr.deleteSession(self.htr_session)
+        models.htr.deleteSession(self.htr_session)
       MyLogger.participants.remove(self)
 
 
@@ -277,50 +291,114 @@ class RouterConnection(SocketConnection):
         print 'Router', repr(info)
 
 
+
+
+class Models:
+  def __init__(self, config_fn):
+    self.config = to_utf8(json.load(open(config_fn)))
+    print >> sys.stderr, "config", json.dumps(self.config)
+ 
+  @timer('create_plugins')
+  def create_plugins(self):
+    start_time = datetime.datetime.now()
+    params = self.config["text-processor"]["parameters"] if "parameters" in self.config["text-processor"] else "" 
+    self.tokenizer_plugin = TextProcessorPlugin(self.config["text-processor"]["module"], params)
+    self.tokenizer_factory = self.tokenizer_plugin.create()
+    if not self.tokenizer_factory: raise Exception("Tokenizer plugin failed")
+    self.tokenizer_factory.setLogger(logger)
+    self.tokenizer = self.tokenizer_factory.createInstance()
+    if not self.tokenizer: raise Exception("Tokenizer instance failed")
+    elapsed_time = datetime.datetime.now() - start_time
+    print "TIME:%s loaded:%s" % ("tokenizer", fmt_delta(elapsed_time))
+
+    start_time = datetime.datetime.now()
+    params = self.config["htr"]["parameters"] if "parameters" in self.config["htr"] else "" 
+    self.htr_plugin = HtrPlugin(self.config["htr"]["module"], params)
+    self.htr_factory = self.htr_plugin.create()
+    if not self.htr_factory: raise Exception("htr plugin failed")
+    self.htr_factory.setLogger(logger)
+    self.htr = self.htr_factory.createInstance()
+    if not self.htr: raise Exception("htr instance failed")
+    elapsed_time = datetime.datetime.now() - start_time
+    print "TIME:%s loaded:%s" % ("htr", fmt_delta(elapsed_time))
+
+  @timer('delete_plugins')
+  def delete_plugins(self):
+    self.tokenizer_factory.deleteInstance(self.tokenizer);
+    self.tokenizer_plugin.destroy(self.tokenizer_factory)
+    self.tokenizer, self.tokenizer_factory = None, None
+    del self.tokenizer_plugin
+  
+    self.htr_factory.deleteInstance(self.htr);
+    self.htr_plugin.destroy(self.htr_factory)
+    self.htr, self.htr_factory = None, None
+    del self.htr_plugin
+  
+
+
 # Create tornadio router
 CasmacatRouter = TornadioRouter(RouterConnection)
+
 
 if __name__ == "__main__":
     from sys import argv
     import logging
     import atexit
+    import getopt 
 
-    logfn = "casmacat-htr-server.log"
-    try: 
-      logfn = models.config["server"]["logfile"]
-    except:
-      pass
-    logfd = codecs.open(logfn, "a", "utf-8")
+    try:
+      opts, args = getopt.getopt(sys.argv[1:], "hl:c:", ["help", "logfile=", "config="])
+    except getopt.GetoptError as err:
+      # print help information and exit:
+      print >> sys.stderr, str(err) # will print something like "option -a not recognized"
+      usage()
+      sys.exit(2)
+    log_fn = None
+    config_fn = None
+    for o, a in opts:
+      if o == "-v":
+        verbose = True
+      elif o in ("-c", "--config"):
+        config_fn = a
+      elif o in ("-l", "--logfile"):
+        log_fn = a
+      else:
+        assert False, "unhandled option"
 
+    if not log_fn:
+      try:
+        log_fn = models.config["server"]["logfile"]
+      except:
+        pass
+    if log_fn:
+      logfd = codecs.open(log_fn, "a", "utf-8")
+    else:
+      logfd = codecs.open(os.path.devnull, "a", "utf-8")
 
     logging.getLogger().setLevel(logging.INFO)
 
+    models = Models(config_fn)
+    models.create_plugins()
+    atexit.register(models.delete_plugins)
 
-    
-    tokenizer_plugin = TextProcessorPlugin("plugins/space-tokenizer.so")
-    tokenizer_factory = tokenizer_plugin.create()
-    if not tokenizer_factory: raise Exception("Tokenizer plugin failed")
-    tokenizer_factory.setLogger(logger)
-    tokenizer = tokenizer_factory.createInstance()
-    if not tokenizer: raise Exception("Tokenizer instance failed")
-    
-    htr_plugin = HtrPlugin("plugins/iatros-plugin.so", "-c /home/demo/software/casmacat-server-library/server/htr-models/casmacat/casmacat.conf")
-    htr_factory = htr_plugin.create()
-    if not htr_factory: raise Exception("HTR plugin failed")
-    htr_factory.setLogger(logger)
-    htr = htr_factory.createInstance()
-    if not htr: raise Exception("HTR instance failed")
+    port = 5002
+    try: 
+      port = int(sys.argv[0])
+    except:
+      try:
+        port = models.config["server"]["port"]
+      except:
+        pass
 
     # Create socket application
     application = web.Application(
         CasmacatRouter.apply_routes([]),
         flash_policy_port = 843,
         flash_policy_file = os.path.join(ROOT, 'flashpolicy.xml'),
-        socket_io_port = 7001
+        socket_io_port = port 
     )
+
+    print >> logfd, """/*\n  Casmacat HTR server started on port %d\n  %s\n*/\n\n"config": %s\n\n\n""" % (port, str(datetime.datetime.now()), json.dumps(models.config, indent=2, separators=(',', ': '), encoding="utf-8"))
 
     # Create and start tornadio server
     SocketServer(application)
-
-    del tokenizer, tokenizer_factory, tokenizer_plugin 
-    del htr, htr_factory, htr_plugin
